97e4147c5d1240c65ca3c6b86e9a49a54d46d27c
BiggBoss CCDB <biggboss.ccdb@gmail.com>
2018-06-22 11:39:50 -0700

    Copy converted html entity to clipboard

--- bookmarklet.js
+++ bookmarklet.js
@@ -8,5 +8,14 @@
     var html = text.replace(/[\u00A0-\u2666]/g, function(c) {
         return '\\char{'+c.charCodeAt(0)+'}';
     });
-    alert(html);
+    const input = document.createElement('input');
+    input.style.position = 'fixed';
+    input.style.opacity = 0;
+    input.value = html;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('Copy');
+    document.body.removeChild(input);
+
+    alert('Copied to your clipboard, now go paste in ccdb')
 })();
